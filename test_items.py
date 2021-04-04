7c7cf5e7f6ab8c7fa65fe86a8c127e6b51ef443e
hypno-spira <64416493+hypno-spira@users.noreply.github.com>
2021-04-04 18:39:56 +0700

    changing files

--- test_items.py
+++ test_items.py
@@ -1,2 +1,12 @@
 import pytest
 from selenium import webdriver
+import time
+
+
+def test_find_card_button(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    time.sleep(30)
+    button_card = browser.find_element_by_css_selector(".btn-add-to-basket")
+
+    assert button_card is not None, "button not found"
